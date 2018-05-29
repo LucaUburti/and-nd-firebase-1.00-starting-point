e49f03f448a95790b578166d2ac7d2d1e09a8671
Luca Uburti <luca.uburti@gmail.com>
2018-05-29 22:00:05 +0200

    Saturday lesson completed Added Firebase DB, set up authentication Instead of AuthStateListener we use the new:   FirebaseAuth.getInstance().getCurrentUser() != null to check if we are authenticated

--- app/src/main/java/com/google/firebase/udacity/friendlychat2/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat2/MainActivity.java
@@ -15,7 +15,10 @@
  */
 package com.google.firebase.udacity.friendlychat2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -29,8 +32,21 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,8 +62,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mPhotoPickerButton;
     private EditText mMessageEditText;
     private Button mSendButton;
-
+    private FirebaseAuth mFirebaseAuth;
+    private static final int RC_SIGN_IN = 123;
     private String mUsername;
+
+    private FirebaseDatabase mFirebaseDatabase;    //il DB
+    private DatabaseReference mMessagesDatabaseReference;    //reference verso una parte specifica del DB
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +75,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mUsername = ANONYMOUS;
+
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -105,11 +131,82 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO: Send messages on click
-
+                FriendlyMessage friendlyMessage = new FriendlyMessage(mMessageEditText.getText().toString(), mUsername, null);
+                mMessagesDatabaseReference.push().setValue(friendlyMessage);
                 // Clear input box
                 mMessageEditText.setText("");
             }
         });
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkIfLoggedIn();
+    }
+
+    private void checkIfLoggedIn() {
+        if (mFirebaseAuth.getCurrentUser() != null) {
+            // already signed in
+            Toast.makeText(this, "You are logged in!", Toast.LENGTH_LONG).show();
+            OnSignedInInitialize(mFirebaseAuth.getCurrentUser().getDisplayName());
+        } else {
+            onSignedOutCleanup();
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+//                                    new AuthUI.IdpConfig.PhoneBuilder().build(),
+                                    new AuthUI.IdpConfig.GoogleBuilder().build()))
+//                                    new AuthUI.IdpConfig.FacebookBuilder().build(),
+//                                    new AuthUI.IdpConfig.TwitterBuilder().build()))
+                            .setIsSmartLockEnabled(false)
+                            .build(), RC_SIGN_IN);
+        }
+    }
+
+
+    private void OnSignedInInitialize(String displayName) {
+        mUsername = displayName;
+        mMessagesDatabaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                //chiamato appena il listener viene agganciato + ogni volta che appare un nuovo dato
+                FriendlyMessage friendlyMessage = dataSnapshot.getValue(FriendlyMessage.class); //deserializzo dal DB sul mio POJO
+                mMessageAdapter.add(friendlyMessage); //lo visualizzo
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                //dato esistente variato
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                //dato esistente cancellato
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                //dato esistente sposato
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //errore in scrittura (solitamente problemi su permessi)
+            }
+        });
+
+    }
+
+    private void onSignedOutCleanup() {
+        mUsername = ANONYMOUS;
+        mMessageAdapter.clear();
+//        mMessagesDatabaseReference.addChildEventListener(null);
+
     }
 
     @Override
@@ -120,7 +217,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "dentrooooooooooo", Toast.LENGTH_SHORT).show();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "cancellatoooooo", Toast.LENGTH_SHORT).show();
+                    finish();
+                    //return;
+                }
+
+//                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+//                    Toast.makeText(this, "mancailnetwoooooork", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+
+                Toast.makeText(this, "erroresconosciutoooooo", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.sign_out_menu) {
+
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                public void onComplete(@NonNull Task<Void> task) {                 // user is now signed out
+                    //startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    //finish();
+                    Toast.makeText(MainActivity.this, "Logged out! ", Toast.LENGTH_LONG).show();
+                    checkIfLoggedIn();
+                }
+            });
+        }
         return super.onOptionsItemSelected(item);
     }
 }
